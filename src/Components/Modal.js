b5460a03b453b885fdd3041561ff38f4d97cf32d
albouchihoussem <albouchihoussemendasimplon@gmail.com>
2021-06-03 09:35:20 +0100

    updatemodif

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import {useState} from 'react';
 import {Button,Modal } from 'react-bootstrap';
+import axios from "axios";
+
+
+
 
 function Update({el,handleSubmit}) {
   //modal
@@ -17,8 +21,17 @@ const handleShow = () => setShow(true);
           imdbRating: el.imdbRating
           
          });
+//update Function//////////////////////
 
- 
+         const handleUpdate = async (id) => {
+          await axios.put(`http://localhost:3008/posts/${id}`,input1)
+          .then(response => {console.log('Status:' , response.status)
+                             console.log('Data : ', response.data)
+                             setInput1(response.data)})
+          .catch(error => console.error('something went wrong', error)
+          )}
+///////////////////////////////////////
+
 
           return(  
             <div>
@@ -31,12 +44,14 @@ const handleShow = () => setShow(true);
       
               
           <form onSubmit={handleSubmit} >
-                <input defaultValue={el.Images}  onChange= { (e) => setInput1 ({...input1, images: e.target.value}) } type='text' name='images' placeholder='enter image link' /><br/>
+                <input defaultValue={el.Images}  onChange= { (e) => setInput1 ({...input1, Images: e.target.value}) } type='text' name='images' placeholder='enter image link' /><br/>
                 <input defaultValue={el.name} onChange= { (e) => setInput1 ({...input1, name: e.target.value}) } type='text' name='name' placeholder='enter movie/serie title' /> <br/>
                 <input defaultValue={el.category}  onChange= { (e) => setInput1 ({...input1, category: e.target.value}) } type='text' name='category' placeholder='enter movie/serie category' /><br/>
                 <input defaultValue={el.imdbRating}  onChange= { (e) => setInput1 ({...input1, imdbRating: e.target.value}) }  type='text' name='imdbRating' placeholder='enter movie/serie rating' /><br/>
 
-            <button type="submit" >Submit</button>
+                <div classname='submitButton'>
+                        <button onClick={()=>handleUpdate(el.id)} >Update</button>
+                        </div>
 
 
    {/* <button type="remove" form="form1" value="Remove" onClick={handleremove}>Delete</button> */}
